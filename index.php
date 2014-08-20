115f87a4ee0bbe7a43cef648f552de90ae46b6f8
hungnd <hungnd@smartosc.com>
2014-08-20 18:09:24 +0700

    add skin for admin pages

--- index.php
+++ index.php
@@ -3,6 +3,32 @@ include "config.php";
 include "functions.php";
 session_start();
 if($_SESSION['login_'] <> 'yes'){
-	header("Location: login.php");
-	die();
+//	header("Location: login.php");
+//	die();
 }
+# scan for modules
+$menus = array();
+$modules = array();
+$scanResults = scandir("modules/");
+foreach($scanResults AS $scanResult) {
+	if (is_dir($scanResult)){
+		if (file_exists($scanResult.'/menu.php')){
+			include_once $scanResult.'/menu.php';
+			$menus = array_merge($menus,$menu);
+		}
+		if(file_exists($scanResult."/".$scanResult.'.php')){
+			$modules[$scanResult] = array(
+				"name" => $scanResult,
+				"file" => $scanResult.'/'.$scanResult.'.php',
+			);
+		}
+	}
+}
+
+#catch the current module
+$curModule = $modules[$_GET['module']];
+$curAction = $_GET['action'];
+
+# we are ok now, lets include the skin.
+
+include "skin/index.php";
