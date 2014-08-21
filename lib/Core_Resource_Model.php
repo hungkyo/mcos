a79c2113d9b7283d1213ea52632b51186942dcfe
hungnd <hungnd@smartosc.com>
2014-08-21 18:00:52 +0700

    fix core resource model..

--- lib/Core_Resource_Model.php
+++ lib/Core_Resource_Model.php
@@ -10,6 +10,7 @@ Class Core_Resource_Model
 	private $curPage = 1;
 	private $pageSize = 10;
 	private $_collection = array();
+
 	public function __construct()
 	{
 		if (!$this->mysql) {
@@ -30,14 +31,45 @@ Class Core_Resource_Model
 		return $this;
 	}
 
-	public function load($val='', $key = 'entity_id')
+	public function addFilter($filter)
 	{
-		$startResult = ($this->curPage-1)*$this->pageSize;
-		$select = implode($this->_selectedAttribute, ',');
-		$where = array("1=1");
-		foreach($this->_filterAttribute AS $filter){
-			// or => array('key' => 'val')
+		$this->_filterAttribute[] = $filter;
+		return $this;
+	}
+
+	public function addSelect($attribute)
+	{
+		$this->_selectedAttribute[] = $attribute;
+		return $this;
+	}
+
+	public function load($val = '', $key = 'entity_id')
+	{
+		$startResult = ($this->curPage - 1) * $this->pageSize;
+		$select = array();
+		foreach($this->_selectedAttribute AS $sel){
+			$select[] = 'v.'.$sel;
 		}
+		$select = implode($select, ',');
+		$where = array("e.entity_id=v.entity_id");
+		if(count($this->_filterAttribute) >= 1){
+			foreach ($this->_filterAttribute AS $filter) {
+				// or => array('key' => 'val','key2' => 'val2')
+				// and => array('key' => 'val','key2' => 'val2')
+				foreach($filter AS $cond => $fil){
+					$whereTemp = array();
+					foreach($fil AS $key=>$val){
+						$whereTemp[] = "{$key}=".var_export($val,true);
+					}
+					$whereTemp = '('.implode(" $cond ", $whereTemp).')';
+				}
+				$where[] = $whereTemp;
+			}
+		}else{
+			$where[] = "$key = ".var_export($val,true);
+		}
+		$where = implode(" AND ",$where);
+		var_dump($where);
 		$q = $this->mysql->query("SELECT {$select} FROM
 		{$this->_table}entity AS e,
 		{$this->_table}eav_val AS v
