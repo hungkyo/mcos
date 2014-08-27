c64c6ec48ec353d3e33ee026d12f701319f5b28a
Hung Nguyen Duy <hungkyo.998@gmail.com>
2014-08-27 22:49:44 +0700

    log POST

--- exec/git.php
+++ exec/git.php
@@ -1,5 +1,5 @@
 <?php
 file_put_contents("x.txt",var_export($_POST,true));
 if ($_POST['payload']) {
-	shell_exec('cd /var/www/mcos && git pull');
+	var_dump(shell_exec('cd /var/www/mcos && git pull'));
 }
