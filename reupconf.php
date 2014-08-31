120198dcf78e661b64fc882d1ee216aa96d4ce3c
Hung Nguyen Duy <hungkyo.998@gmail.com>
2014-09-01 01:18:52 +0700

    custom log for apache

---
+++ reupconf.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+#
+# load our bootstrap
+# but wait... dafuq is a bootstrap?
+$thisDir = dirname(__FILE__). '/';
+define('thisDir',$thisDir);
+foreach (glob(thisDir."lib/*.php") AS $file) {
+	include_once $file;
+}
+include $thisDir."config.php";
+include $thisDir."functions.php";
+$mysql = getModel('DB');
+$mysql->connect($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
+
+include $thisDir.'modules/Servers/serverModel.php';
+include $thisDir.'modules/Domains/domainModel.php';
+
+$domain = getModel('domain');
+$domains = $domain
+	->getAll()
+	->load();
+
+foreach ($domains AS $domain) {
+	$server = getModel('server')->load($domain->getData('server'));
+	$install = getModel('installWP');
+	$install->setData('domain', $domain->getData('name'));
+	$install->setData('server', $server);
+	$install//->connectDB()
+		->connectFTP()
+		//->mkDB()
+		//->dumpDB()
+		//->mkTempDir()
+		->mkConfFile()
+		//->uploadClient()
+		->uploadConfFile();
+//	$domain->setData('installed', 1)
+//		->save();
+	echo $domain->getData('name')."\r\n";
+}
