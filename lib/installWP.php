1fffe8eadb49d3aae113d98a6f14c8fcbf11e350
Hung Nguyen Duy <hungkyo.998@gmail.com>
2014-09-03 15:42:46 +0700

    fix cleanup function

--- lib/installWP.php
+++ lib/installWP.php
@@ -151,7 +151,7 @@ class installWP
 		return $this;
 	}
 	public function cleanUp(){
-		shell_exec("rm -f ".thisDir.'temp/'.$this->domain);
+		shell_exec("rm -rf ".thisDir.'temp/'.$this->domain);
 		return $this;
 	}
 }
