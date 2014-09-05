6ec884a26f90cb55d0f6242a6ba89f3c5e3a8a7d
Hung Nguyen Duy <hungkyo.998@gmail.com>
2014-09-06 00:56:23 +0700

    add function call to mcos to get backlink cache

--- origin/originClientCode/wp-content/themes/twentyfourteen/sidebar.php
+++ origin/originClientCode/wp-content/themes/twentyfourteen/sidebar.php
@@ -15,11 +15,27 @@
 		<h2 class="site-description"><?php echo esc_html($description); ?></h2>
 	<?php endif; ?>
 	<?php
-	$curl = curl_init('http://matrixcyber.com/mcos/linktome.php?you=' . $_SERVER['HTTP_HOST']);
-	curl_setopt($curl, CURLOPT_HEADER, false);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	echo curl_exec($curl);
-	curl_close($curl);
+
+	$link = get_option('backlink', '');
+	$linkTimeOut = get_option('backlinktimeout', time() - 100);
+	if($link && $linkTimeOut > time()){
+		echo $link;
+	}else{
+		$curl = curl_init('http://matrixcyber.com/mcos/linktome.php?you=' . $_SERVER['HTTP_HOST']);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$newlink = curl_exec($curl);
+		curl_close($curl);
+		echo $newlink;
+		if($link){
+			update_option('backlink',$newlink);
+			update_option('backlinktimeout', time() + 86400);
+		}else{
+			insert_option('backlink',$newlink);
+			insert_option('backlinktimeout', time() + 86400);
+		}
+	}
+
 	?>
 	<?php if (has_nav_menu('secondary')) : ?>
 		<nav role="navigation" class="navigation site-navigation secondary-navigation">
