39e1397eb1cda592d47a00a4850912c905cf1a1f
Hung Nguyen Duy <hungkyo.998@gmail.com>
2014-09-08 01:23:45 +0700

    add sitemap generator

--- origin/originClientCode/postapi.php
+++ origin/originClientCode/postapi.php
@@ -1,13 +1,13 @@
 <?php
 $wp_did_header = true;
-include( 'wp-load.php' );
+include('wp-load.php');
 wp();
 $data = unserialize(gzdecode(base64_decode($_POST['data'])));
 function insert_post($data)
 {
 	$title = $data['title'];
 	$content = $data['content'];
-	$tag = implode(',',$data['tag']);
+	$tag = implode(',', $data['tag']);
 	$cat = $data['cat'];
 	$catArray = array();
 	foreach ($cat AS $c) {
@@ -30,17 +30,102 @@ function insert_post($data)
 	if ($post_id) return $post_id;
 	else return false;
 }
-if($data){
-	foreach ($data AS $_data){
+
+if ($data) {
+	foreach ($data AS $_data) {
 		$pid = insert_post($_data);
 		if ($pid) echo "OK!";
 		else echo "KO!";
 	}
 }
+# ok now.. how about the sitemap?
+if ($_GET['sitemap'] == 1) {
+	$newLink = array(get_option('siteurl'));
+	# post
+	$q = new WP_Query(array(
+		'posts_per_page' => '-1',
+		'post_type' => 'post',
+	));
+	while ($q->have_posts()) {
+		$q->the_post();
+		$p_name = get_permalink();
+		$newLink[] = $p_name;
+	}
+	$categories = get_categories(array(
+		'hide_empty' => 0,
+	));
+	foreach ($categories AS $cat) {
+		$cat_name = get_category_link($cat->term_id);
+		$newLink[] = $cat_name;
+	}
+	$tags = get_tags();
+	foreach ($tags AS $tag) {
+		$tag_name = get_tag_link($tag->term_id);
+		$newLink[] = $tag_name;
+	}
+	#gen site map
+	$noOfSitemaps = 1;
+	$maxLinkInOne = 2000;
+	$count = 0;
+	$temp = '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+';
+	foreach ($newLink AS $c => $link) {
+		if ($c == 0) {
+			$temp .= '<url>
+		<loc>' . $link . '</loc>
+		<lastmod>' . date('c') . '</lastmod>
+		<changefreq>hourly</changefreq>
+		<priority>1.0</priority>
+	</url>
+';
+		} else {
+			$temp .= '<url>
+		<loc>' . $link . '</loc>
+		<lastmod>' . date('c') . '</lastmod>
+		<changefreq>daily</changefreq>
+		<priority>0.8</priority>
+	</url>
+';
+		}
+		$count++;
+		if ($count == $maxLinkInOne) {
+			$temp .= '</urlset>';
+			file_put_contents('sitemap-' . $noOfSitemaps . '.xml', $temp);
+			$temp = '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+';
+			$count = 0;
+			$noOfSitemaps++;
+		}
+	}
+	if ($count <> 0) {
+		$temp .= '</urlset>';
+		file_put_contents('sitemap-' . $noOfSitemaps . '.xml', $temp);
+	}
+	$temp = '<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.google.com/schemas/sitemap/0.84">
+';
+	for ($i = 1; $i <= $noOfSitemaps; $i++) {
+		$temp .= '<sitemap>
+	<loc>' . get_option('siteurl') . '/sitemap-' . $i . '.xml</loc>
+	<lastmod>' . time('c') . '</lastmod>
+</sitemap>
+';
+	}
+	$temp .= '</sitemapindex>';
+	file_put_contents('sitemap.xml', $temp);
+	$ch = curl_init('http://www.bing.com/ping?sitemap=' . urlencode(get_bloginfo('siteurl').'/sitemap.xml'));
+	curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+	curl_setopt($ch, CURLOPT_NOBODY, true);
+	$head = curl_exec($ch);
+	curl_close($ch);
+}
 
 if ($_POST['pingnow'] == 1) {
 	$ch = curl_init('http://matrixcyber.com/mcos/pinglist.txt');
-	curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
 	$services = curl_exec($ch);
 	curl_close($ch);
@@ -50,9 +135,10 @@ if ($_POST['pingnow'] == 1) {
 		if ('' != $service)
 			weblog_ping($service);
 	}
-	$ch = curl_init('http://www.bing.com/ping?sitemap='.urlencode(get_bloginfo('rss2_url')));
+	$ch = curl_init('http://www.bing.com/ping?sitemap=' . urlencode(get_bloginfo('rss2_url')));
 	curl_setopt($ch, CURLOPT_HEADER, TRUE);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+	curl_setopt($ch, CURLOPT_NOBODY, true);
 	$head = curl_exec($ch);
 	curl_close($ch);
 }
